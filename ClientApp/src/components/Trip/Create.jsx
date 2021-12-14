bf108e626923ac6ad0d89eb8724111266020c8f7
ericKristiansen <eric.kristiansen@mail.com>
2021-12-14 11:29:12 -0800

    trips content

---
+++ ClientApp/src/components/Trip/Create.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+export class Create extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            description: '',
+            dateStarted: null,
+            dateCompleted: null
+        }
+    }
+
+    render() {
+        return (
+            <div className="trip-form" >
+                <h3>Add New Trip</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Trip Name: </label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label>Description: </label>
+                        <textarea type="text" className="form-control" />
+                    </div>
+
+                    <div className="row">
+                        <div className="col col-md-6 col-sm-6 col-xs-12">
+                            <div className="form-group">
+                                <label>Start Date: </label>
+                                <input type="date" className="form-control" />
+                            </div>
+                        </div>
+
+                        <div className="col col-md-6 col-sm-6 col-xs-12">
+                            <div className="form-group">
+                                <label>Date of completion: </label>
+                                <input type="date" className="form-control" />
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+        );
+    }
+
+}
